70b906253de9e2e215ea42b06f60fcb9b86ad9b3
angga.widianto <angga.widianto@raharja.info>
2022-02-11 16:47:53 +0700

    setup routes tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,7 +4,12 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { errorHandler } from './middlewares/error-handler';
+import { currentUser } from './middlewares/current-user';
 import { NotFoundError } from './errors/not-found-error';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -12,6 +17,12 @@ app.use(json());
 app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== 'test' })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('/*', async () => {
   throw new NotFoundError();
---
+++ tickets/src/routes/update.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
+import { NotFoundError } from '../errors/not-found-error';
+import { requiredAuth } from '../middlewares/require-auth';
+import { NotAuthorizedError } from '../errors/not-authorized-error';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.put(
+  '/api/tickets/:id',
+  requiredAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = await Ticket.findById(req.params.id);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    ticket.set({
+      title,
+      price,
+    });
+    await ticket.save();
+
+    res.send(ticket);
+  }
+);
+
+export { router as updateTicketRouter };
